583874b6d05a61ad9adf804b7b2406438b0e1a70
Jacobtread <jacobtread@gmail.com>
2025-05-04 16:27:56 +1200

    fix: remove broken test

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,3 @@
 pub mod layer;
 pub mod queue;
 pub mod runtime;
-
-#[cfg(test)]
-mod test {
-
-    #[tokio::test]
-    async fn main() {
-        use crate::runtime::{HttpRequest, SvelteServerRuntime};
-        use std::path::Path;
-
-        let server_path = Path::new(env!("CARGO_MANIFEST_DIR")).join("build");
-        let handle = SvelteServerRuntime::create(server_path).unwrap();
-
-        let response = handle
-            .request(HttpRequest {
-                method: "GET".to_string(),
-                headers: Vec::new(),
-                url: "http://localhost:5173/sverdle".to_string(),
-                body: None,
-            })
-            .await
-            .unwrap();
-
-        println!("response: {response:?}");
-    }
-}
